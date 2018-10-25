f068df07f4059eaa6dd18b58fd54799543f7b3f5
mjg79 <mahdi.javan.gh@gmail.com>
2018-10-25 11:11:34 +0330

    added

---
+++ gilg/src/ScoreBoard.java
@@ -0,0 +1,20 @@
+public class ScoreBoard {
+    private int score = 0;
+    private int money = 0;
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setScore(City city) {
+        this.score = city.getScore;
+    }
+
+    public void setMoney(City city) {
+        this.money = city.getMoney();
+    }
+}
