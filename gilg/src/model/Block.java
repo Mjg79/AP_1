f3d84519e6a100a4804049ddcdabc4f1c7b83e76
alirezadizaji <alirezadizaji@yahoo.com>
2018-10-25 19:21:03 +0330

    "updated again"

--- gilg/src/model/Block.java
+++ gilg/src/model/Block.java
@@ -8,24 +8,43 @@ import model.Residency.Home;
 import java.util.ArrayList;
 
 public class Block {
-    private int level ;
-    private ArrayList <Building> buildings = new ArrayList<>();
+    private int level = 1;
+    private ArrayList<Building> buildings = new ArrayList<>();
+    private int capacity = 15;
     boolean flag_defense = false;
-    int ID ;
+    private Army army = new Army();
+    int ID;
 
-    public void addHome(int numOfFloors , int numOfUnit){
+    public void UpgradeBlock(){
+        capacity += 5;
+        if(level < 3)
+        level += 1;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public boolean canAdd(){
+        if(capacity > buildings.size())
+            return true;
+        return false;
+    }
+
+    public void addHome(int numOfFloors, int numOfUnit) {
         Home home = new Home();
-        for (int i = 0 ; i < numOfFloors ; i++) {
+        for (int i = 0; i < numOfFloors; i++) {
             home.addFloor();
         }
         for (Floor f : home.getFloors()) {
             f.addUnit(numOfUnit);
         }
-      buildings.add(home);
+        buildings.add(home);
     }
 
     public void addArmy() {
-        buildings.add(new Army());
+        army = new Army();
+        buildings.add(army);
     }
 
     public void addDefense() {
@@ -40,11 +59,32 @@ public class Block {
     public void UpgradeHome(int homeID) {
         Home home = (Home) buildings.get(homeID);
         home.addFloor();
-        for(Floor f : home.getFloors())
+        for (Floor f : home.getFloors())
             f.addUnit(1);
     }
 
-    public void UpgradeArmy(){
-
+    public void UpgradeArmy() {
+        if (army != null)
+            army.addLevel();
     }
+
+    public void UpgradeBazaar(int id) {
+        Bazaar bazaar = (Bazaar) buildings.get(id);
+        buildings.remove(id);
+        bazaar.addLevel();
+        buildings.add(id, bazaar);
+    }
+
+    public void UpgradeDefense(int id) {
+        Defense defense = null;
+        for (Building b : buildings) {
+            if (b.getID() == id) {
+                defense = (Defense) b;
+                defense.addLevel();
+                break;
+            }
+        }
+    }
+
+    public void remove
 }
