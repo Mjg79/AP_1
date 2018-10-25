2b79fa6300cf699f5f897c400d488b378d9d5e84
mjg79 <mahdi.javan.gh@gmail.com>
2018-10-25 10:37:49 +0330

    main classes added

---
+++ gilg/src/Building.java
@@ -0,0 +1,39 @@
+public abstract class Building {
+    private int ID;
+    private int blockID;
+    int day=1;
+    int money = 0;
+    int score = 0;
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getBlockID() {
+        return blockID;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setBlockID(int blockID) {
+        this.blockID = blockID;
+    }
+}
---
+++ gilg/src/Floor.java
@@ -0,0 +1,10 @@
+public class Floor {
+    private int numOFUnits=1;
+
+    public int Persons() {
+        return numOFUnits*5;
+    }
+    public void addUnit(){
+        this.numOFUnits+=1;
+    }
+}
